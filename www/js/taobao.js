dbd95552d3bfb1e9f290f593eb39ab389be706b7
jcoy <oyanglulu@gmail.com>
2012-08-09 11:46:04 +0800

    sever delete

--- www/js/taobao.js
+++ www/js/taobao.js
@@ -1,18 +1,43 @@
-var taobaoURL = "https://eco.taobao.com/router/rest";
-var sufParam='&format=json&v=2.0&callback=?';
+// real
+taobaoURL = "https://eco.taobao.com/router/rest";
+// Sandbox
+// taobaoURL ='http://gw.api.tbsandbox.com/router/rest';
+// authURL = 'https://oauth.tbsandbox.com/authorize';
+
+sufParam='&format=json&v=2.0&callback=?';
+// serviceURL = 'http://huanduoduo.com/taobao/'
+// loginURL = 'http://huanduoduo.com/login/taobao/'
+serviceURL = 'http://127.0.0.1:8000/taobao/'
+loginURL = 'http://127.0.0.1:8000/login/taobao/'
 var access_token=null;
 var company=null;
-var client_id = '12643303';
-var authurl = 'https://oauth.taobao.com/authorize?response_type=code&client_id='+client_id+'&redirect_uri=urn:ietf:wg:oauth:2.0:oob&state=1212&scope=item&view=wap'
+client_id = '12633133';
+// var authurl = 'https://oauth.taobao.com/authorize?response_type=code&client_id='+client_id+'&redirect_uri=urn:ietf:wg:oauth:2.0:oob&state=1212&scope=item&view=wap';
 //
 //$( document ).bind( "mobileinit", function() {
 //                   // Make your jQuery Mobile framework configuration changes here!
 //                   
 //                   $.mobile.allowCrossDomainPages = true;
 //                   });
-var serviceURL = 'http://huanduoduo.com/taobao/'
-//var serviceURL = 'http://127.0.0.1:8000/taobao/'
 
+//var serviceURL = 'http://127.0.0.1:8000/taobao/'
+var status_msg={
+  'TRADE_NO_CREATE_PAY':'没有创建支付宝交易',
+  'WAIT_BUYER_PAY':'等待买家付款',
+  'WAIT_SELLER_SEND_GOODS':'等待卖家发货,即:买家已付款',
+  'WAIT_BUYER_CONFIRM_GOODS':'等待买家确认收货,即:卖家已发货',
+  'TRADE_BUYER_SIGNED':'买家已签收',
+  'TRADE_FINISHED':'交易成功',
+  'TRADE_CLOSED':'交易关闭',
+  'TRADE_CLOSED_BY_TAOBAO':'交易被淘宝关闭'
+}
+
+function locationChange(loc) {
+  console.log("location change",loc);
+  if(loc=="http://geogeo.github.com/taozhuli/"){
+    window.plugins.childBrowser.close();
+    refresh();}
+}
 
 function updatePostFee(event){
   event.preventDefault();
@@ -21,7 +46,7 @@ function updatePostFee(event){
   $this=$(this);
   var formData = $this.serialize();
   console.log($this);
-  $.getJSON('https://eco.taobao.com/router/rest',formData+'&method=taobao.trade.postage.update&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
+  $.getJSON(taobaoURL,formData+'&method=taobao.trade.postage.update&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
     console.log(data.error_response);
     if(data.error_response){
       $('#error_dialog').find('h2').text(data.error_response.sub_msg);
@@ -45,76 +70,88 @@ function updatePostFee(event){
   });
   return false;
 };
-function logout(){
-//    localStorage.clear();
-    //    alert('logout');
-    var cb = window.plugins.childBrowser;
-    
-    if(cb != null) {
-        cb.showWebPage("http://google.com");
-    
-    
-}
-function checkauth($this){
 
-    src=$this.attr('src');
-    console.log('src',src);
-    console.log('iframe',$('#tblogin > iframe > html').find('.box-bd p').text());
-    if($.inArray('agreement=true',src.split('&'))){
-        console.log($('#tblogin > iframe > html').find('.box-bd p').text());
-        console.log($this.find('.box-bd p').text());
-    }
+
+function logout(){
+  localStorage.removeItem("token")
+  // var logoutwin= window.open("http://huanduoduo.com/account/logout",'登出', 'width=400,height=600,scrollbars=yes');
+  //  logoutwin.onload=self.close();
+  var cb = window.plugins.childBrowser;
+  if(cb){
+    cb.showWebPage(loginURL);
+    cb.onLocationChange = function(loc){locationChange(loc);};
+  }
+  else{ 
+var loginwin= window.showModalDialog(loginURL,'登入', 'width=400,height=600,scrollbars=yes');
+      // loginwin.onunload =function(){console.log('close');}
+      console.log('close');
+      refresh()
+      }
 }
 
 function requestToken(){
-    var event = 'onload="checkauth($(this))"';
-    var html = '<iframe src="'+authurl+'" width="100%" '+event+' height="600px"><p>Your browser does not support iframes.</p></iframe>'
-    $tblogin=$('#tblogin').html(html);
-    $.mobile.changePage('#login');
-//  $.getJSON(serviceURL+ 'get_accesstoken', 'callback=?',function(data) {
-//    if (!data.token)
-//      window.location='http://huanduoduo.com/login/taobao/';
-//    localStorage.setItem("token",data.token);
-//    access_token=data.token
-//    console.log(access_token);
-//    getSoldList();
-//  });
-    
+  
+  $.getJSON(serviceURL+ 'get_accesstoken', 'callback=?',function(data) {
+    if (!data.token){
+      var cb = window.plugins.childBrowser;
+      if(cb){
+        cb.showWebPage(loginURL);
+        cb.onLocationChange = function(loc){locationChange(loc);};
+      }
+      else{ 
+        var loginwin= window.showModalDialog(loginURL,'登入', 'width=400,height=600,scrollbars=yes');
+            // loginwin.showModalDialog =function(){console.log('close');}
+        refresh();
+      }
+    }else{
+      localStorage.setItem("token",data.token);
+      access_token=data.token
+      console.log(access_token);
+      getSoldList();
+    }
+  });
+  
 }
 function getSoldList() {
-
-  $.getJSON('https://eco.taobao.com/router/rest','method=taobao.trades.sold.get&access_token='+getToken()+'&format=json&v=2.0&fields=seller_nick,buyer_nick,title, type, created, tid, seller_rate, buyer_rate, status, payment, discount_fee, adjust_fee, post_fee, total_fee, pay_time, end_time, modified, consign_time, buyer_obtain_point_fee, point_fee, real_point_fee, received_payment, commission_fee, pic_path, num_iid, num, price, cod_fee, cod_status, shipping_type, receiver_name, receiver_state, receiver_city, receiver_district, receiver_address, receiver_zip, receiver_mobile, receiver_phone,seller_flag,alipay_id,alipay_no,is_lgtype,is_force_wlb,is_brand_sale,buyer_area,has_buyer_message,orders&callback=?',function(data){
+  console.log('geting list');
+  $.getJSON(taobaoURL,'method=taobao.trades.sold.get&access_token='+getToken()+'&format=json&v=2.0&fields=buyer_nick,title,created, tid, status, payment, discount_fee, adjust_fee, post_fee, total_fee, pay_time, end_time, consign_time, received_payment, pic_path, num, price, cod_fee, cod_status, shipping_type, receiver_name, receiver_state, receiver_city, receiver_district, receiver_address, receiver_zip, receiver_mobile, receiver_phone,alipay_id,alipay_no,has_buyer_message,orders&callback=?',function(data){
     //        console.log('getsoldlist',data);
-    if(data.error_response) requestToken();
-    $('#sold_items_list li').remove();
-    sold_items = data.trades_sold_get_response.trades.trade;
     
+    if(data.error_response||!data.trades_sold_get_response) 
+    {
+      requestToken();
+      return;
+    }
+    var sold_items=data.trades_sold_get_response.trades.trade;
+    $('#sold_items_list li').remove();
+    //    sold_items = data.trades_sold_get_response.trades.trade;
+    $('.order-detail').remove();
     $.each(sold_items, function(index, item) {
-      
+        console.log(item);
+      item.status = status_msg[item.status]
       $('#sold_item_template').tmpl(item).appendTo('#sold_items_list');
       $("#detail_page_template").tmpl(item).appendTo('body');
       
       $('#detail_order_template').tmpl(item.orders.order).appendTo('#tid'+item.tid+' section.order.order_list > ul');
     });//end function(index,item)
+    
     $('#sold_items_list').listview('refresh');
     $('.form_postfee').submit(updatePostFee);
-    getCompany();
-    $( '#sold_items_list' ).find('li').bind('click',function(event){
+    $('.order-detail').bind('pageshow',function(){
       token=localStorage.getItem("token");
-      var href =  $(this).attr('ref');
-      console.log(href);
-      var tid=href.substring(4,href.length);
-      console.log("tid",tid);
-      $.getJSON('https://eco.taobao.com/router/rest','tid='+tid+'&fields=trades.buyer_memo&method=taobao.trade.get&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
+      var href =  $(this).attr('id');
+      var tid=href.substring(3,href.length);
+        if(1){
+      $.getJSON(taobaoURL,'tid='+tid+'&fields=trades.buyer_memo&method=taobao.trade.get&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
         
         if(data.error_response){
           $('#error_dialog').find('h2').text(data.error_response.sub_msg||data.error_response.msg);
+          $.mobile.changePage('#home');
           $.mobile.changePage("#error_dialog",
                               {
                                 role:'dialog',
                                 transition: "pop"
                               })
-          return false;
         }else{
           if(data.trade_get_response.trade.buyer_memo)
             $('#memo'+tid).text(data.trade_get_response.trade.buyer_memo);
@@ -122,22 +159,38 @@ function getSoldList() {
             $('#memo'+tid).text('无');
         };
       });
-    });
+        }//end get memo
+
+    }
+                           )
+    console.log('getting company'
+               )
+    getCompany();
+    $.mobile.hidePageLoadingMsg();
+    // $( '#sold_items_list' ).find('li').bind('click',function(event){
     
-    //end each()
+    // });
+    
+    //end getJOSN()
   })
+
 };
 
-
+function refresh(){
+  $.mobile.showPageLoadingMsg();
+  getSoldList();
+  // console.log('refreshed');
+}
 function getToken(){
   if(access_token)
     return access_token;
   else{
     var token= localStorage.getItem("token");
-    if (token)return localStorage.getItem("token");
-    else{
-      requestToken();
-      return false;}}
+    return localStorage.getItem("token");
+    // else{
+    //   requestToken();
+    //   return false;}
+  }
 };
 function getCompany(){
   if(company)return company;
@@ -149,23 +202,21 @@ function getCompany(){
     });
 };
 
-
-
-function   scan(){
+function scan(){
   window.plugins.barcodeScanner.scan(
     function(result) {
       console.log(result);
       if (result.cancelled)
         // navigator.notification.alert("the user cancelled the
         // scan")
-        console.log('user cancel');
+        $.mobile.changePage('#send_goods',{role:'dialog'});
       else
         navigator.notification.confirm(
           '请确认单号是否正确:\n'+result.text,  // mes'sage
           function confirmscan(button){
             if(button==1){
               $('#send_goods form input[name="out_sid"]').val(result.text);
-                                        $.mobile.changePage('#send_goods',{role:'dialog'});
+              $.mobile.changePage('#send_goods',{role:'dialog'});
             }
             else{
               scan();
@@ -174,7 +225,6 @@ function   scan(){
           '扫描成功',            // title
           '确定,重新扫描'          // buttonLabels
         );
-
     },
     function(error) {
       alert("scanning failed: " + error)
@@ -182,53 +232,74 @@ function   scan(){
   );
 
 }
-$( '#home' ).live( 'pageinit',function(event){
-  console.log('init');
-  if (getToken)
-    getSoldList();
-  
-  //when the send goods btn clicked
-  $('.btn_send_goods').live('click',function(){
-                            
+function filter(){
+  $.mobile.changePage("#home");
+  $('input[data-type="search"]').val("等待卖家发货").trigger("change");
+}
+function clearfilter(){
+  $.mobile.changePage("#home");
+  console.log('clear fileter');
+  $('input[data-type="search"]').val("").trigger("change");
+}
+$( '#home' ).live('pagebeforecreate',
+                  function(){
+                    console.log('before');
+                    $.mobile.showPageLoadingMsg();
+                    setTimeout($.mobile.showPageLoadingMsg, 2);
+                  }
+                 )
+
+  .live( 'pageinit',function(event){
+    console.log('init');
+
+    if (getToken){       
+      getSoldList();
+    }
     
-           $('#send_goods form input[name="tid"]').val($(this).attr("name"))
-           var company=getCompany()
-           console.log( company);
-           var select = $('#form_send_goods select');
-           select.empty();
-           $.each(company,function(index,item){
-               select.append(
-                   '<option  value="'+item.code+'" >'+item.name+'</option>'
-               )});
-                            scan();
-  });
-  $('#form_send_goods').submit(function(event){
-    event.preventDefault();
-    token=localStorage.getItem("token");
-    console.log('form not submit');
-    var formData = $(this).serialize();
-    $.getJSON('https://eco.taobao.com/router/rest',formData+'&method=taobao.logistics.offline.send&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
-      console.log(data.error_response);
-      if(data.error_response){
-        $('#error_dialog').find('h2').text(data.error_response.sub_msg||data.error_response.msg);
-        $.mobile.changePage("#error_dialog",
-                            {
-                              role:'dialog',
-                              transition: "pop"
-                            })
-      }else{
-        $.mobile.changePage("#error_dialog",
-                            {
-                              role:'dialog',
-                              transition: "pop"
-                            })
-      };
+    $('.free-delivery').live('click',function(){
+      $(this).closest('input[name=post_fee]').val("0")
+        .closest('form').submit();
+    })
+
+
+    //when the send goods btn clicked
+    $('.btn_send_goods').live('click',function(){
+      $('#send_goods form input[name="tid"]').val($(this).attr("name"))
+      var select = $('#form_send_goods select');
+      if (!select.find('option').length){
+        var company=getCompany()
+        console.log( company);
+        
+        // select.empty();
+        $.each(company,function(index,item){
+          select.append(
+            '<option  value="'+item.code+'" >'+item.name+'</option>'
+          )});
+      }
+      
+      if( window.plugins.barcodeScanner)
+        scan();
+      else
+        $.mobile.changePage('#send_goods',{role:'dialog'});
     });
-    return false;
+    $('#form_send_goods').submit(function(event){
+      event.preventDefault();
+      token=localStorage.getItem("token");
+      
+      var formData = $(this).serialize();
+      console.log('form will submit',formData);
+      // $.getJSON('https://eco.taobao.com/router/rest',formData+'&method=taobao.logistics.offline.send&access_token='+token+'&format=json&v=2.0&callback=?',function(data){
+        // console.log('sendgood',data);
+      //   if(data.error_response){
+      //     $('#error_dialog').find('h2').text(data.error_response.sub_msg||data.error_response.msg);
+      //   }
+      //     $.mobile.changePage("#error_dialog",
+      //                         {
+      //                           role:'dialog',
+      //                           transition: "pop"
+      //                         })
+      // });
+      return false;
+    });
+    // $('#buck_action').bind('click',function(){
   });
-  // $('#buck_action').bind('click',function(){
-  
-});
-
-
-
